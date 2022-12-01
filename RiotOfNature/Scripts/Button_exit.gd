6bda685d3765984b6351ba1df4a9863d9bfae852
Alex Elias <alex.elias@edu.pucrs.br>
2022-12-01 20:36:22 -0300

    add new hud next map

---
+++ RiotOfNature/Scripts/Button_exit.gd
@@ -0,0 +1,9 @@
+extends Button
+
+
+func _ready():
+	self.connect("pressed", self, "_button_pressed")
+	pass # Replace with function body.
+
+func _pressed():
+		get_tree().change_scene_to(load('res://Scene/HUD.tscn'))
