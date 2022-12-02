87476e3f6d0910d33b774181113b67d5ade0a605
Alex Elias <alex.elias@edu.pucrs.br>
2022-12-02 15:35:56 -0300

    fix sound

--- RiotOfNature/Scripts/HUDFases.gd
+++ RiotOfNature/Scripts/HUDFases.gd
@@ -6,7 +6,6 @@ onready var starsMap2 = get_node("TextureRect3")
 onready var map2 = get_node("TextureButton2")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(progress.fases)
 	var resMap1 = load("res://assets/stars/"+str(progress.fases.fase1.bestStars)+"star.png")
 	var resMap2 = load("res://assets/stars/"+str(progress.fases.fase2.bestStars)+"star.png")
 	starsMap1.texture = resMap1
